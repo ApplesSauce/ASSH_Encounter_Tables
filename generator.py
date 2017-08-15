0d0cef0cd9fce319b9896fcd16669203551e2960
Scott <sjs1512@gmail.com>
2017-08-14 22:01:36 -0400

    adjusted json files, new tables, logic in gen script
    Changed json files to remove string-ed numbers, adjust die type and number to make logic easier for generating number of monsters encounted. Added some logic to the generation file to filter through the different encounter types.

--- generator.py
+++ generator.py
@@ -3,34 +3,45 @@ from die import Die
 from collections import OrderedDict
 
 
-def encounter_gen(location, die_role):
-    with open(tables+location.replace(" ", "_").replace("'", "").lower()+'.json') as data_file:
+def encounter_gen(file_location, encounter_location, die_role):
+    with open(file_location + encounter_location.replace(" ", "_").replace("'", "").lower()+'.json') as data_file:
         data = json.load(data_file)
     for entry in data['encounter']:
         if entry['roll'] == die_role:
             encounter_dict = OrderedDict()
+            encounter_dict['terrain'] = data['terrain']
             encounter_dict['encounter'] = entry['value']
             encounter_dict['is_italic'] = entry['italic']
-            encounter_dict['is_bold'] = entry['bold']
             encounter_dict['is_special'] = entry['special']
             encounter_dict['number'] = entry['number']
-            return encounter_dict
+            if 'bold' in entry.keys():
+                encounter_dict['is_bold'] = entry['bold']
+            if entry['rand_num']:
+                encounter_dict['die_type'] = entry['die_type']
+                encounter_dict['die_num'] = entry['die_num']
+                return encounter_dict
+            else:
+                return encounter_dict
 
-tables = "tables\\"
-encounter_location = "test"
 
-encounter = encounter_gen(encounter_location, 3)
+location = "tables\\location\\"
 
-for x, y in encounter.items():
-    print(f'{x:7} \t\t {str(y)}')
+location_choice = "test"
 
+encounter = encounter_gen(location, location_choice, Die(6).three_die_straight())
 
-if encounter['is_italic'] == True:
+if encounter['is_italic']:
     print("Encounter Occurs")
-elif encounter['is_bold'] == True:
-    print("roll 3d6 on Terrain Table")
-elif encounter['is_special'] == True:
-    print("A special encounter has occured")
+    for x, y in encounter.items():
+        print(f'{x:7} \t\t {str(y)}')
+elif encounter['is_bold']:
+    print(f"roll 3d6 on {encounter['encounter']} {encounter['terrain']} Table")
+    terrain = "tables\\terrain\\" + encounter['terrain'] + "\\"
+    terrain_encounter = encounter_gen(terrain, encounter['encounter'], Die(6).three_die_straight())
+    for x, y in terrain_encounter.items():
+        print(f'{x:7} \t\t {str(y)}')
+elif encounter['is_special']:
+    print(f"A special encounter has occurred with {encounter['encounter']}")
 else:
     print("Roll again!")
 
